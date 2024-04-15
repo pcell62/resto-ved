4840c44a10828fefabe43644bcd39abec77a28f9
Priyanshu Kumar <priyanshukumr10714@gmail.com>
2024-04-15 14:05:13 +0530

    Update dependencies in package.json

---
+++ app/(components)/ResturantCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+const RestaurantCard = ({
+  name,
+  description,
+  image,
+}: {
+  name: string;
+  description: string;
+  image: string;
+}) => {
+  return (
+    <div className="max-w-xs rounded overflow-hidden shadow-lg m-4 backdrop-blur-sm">
+      <img className="w-full h-56 object-cover" src={image} alt={name} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <p className="text-gray-700 text-base">{description}</p>
+      </div>
+      <div className="px-6 py-4">
+        <Link href="/order">
+          <button className="flex justify-center items-center gap-2  h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-red-600  hover:shadow-xl p-2 tracking-wider">
+            Order Now
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,116 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
+import { FaArrowRight } from "react-icons/fa";
+import RestaurantCard from "./(components)/ResturantCard";
+import Link from "next/link";
+
+const Page = () => {
+  const indianFoods = [
+    {
+      name: "Butter Chicken",
+      description:
+        "A classic Indian dish made with tender chicken cooked in a creamy tomato sauce.",
+      image:
+        "https://www.177milkstreet.com/assets/site/Recipes/_large/Butter-Chicken.jpg",
+    },
+    {
+      name: "Palak Paneer",
+      description:
+        "A vegetarian dish consisting of paneer (Indian cheese) in a thick paste made from puréed spinach.",
+      image:
+        "https://healthynibblesandbits.com/wp-content/uploads/2020/01/Saag-Paneer-FF.jpg",
+    },
+    {
+      name: "Biryani",
+      description:
+        "A fragrant rice dish cooked with Indian spices, meat (such as chicken or lamb), and vegetables.",
+      image:
+        "https://i.pinimg.com/736x/29/a2/50/29a250fef4c1e5190dc14da037ca751f.jpg",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="mt-20 p-6 max-w-[88rem] m-auto">
+      <div className="relative">
+        <img
+          src="https://www.chenabgourmet.com/wp-content/uploads/2022/05/Blog-Designs-12.png"
+          alt="food-banner"
+          className="w-full rounded-2xl h-[80vh] bg-no-repeat brightness-60 filter hidden md:block shadow-2xl shadow-red-600"
+        />
+        <div className="text-6xl  absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 p-8 pl-14 z-10 hidden md:block backdrop-blur-sm">
+          <div className=" font-bold text-black drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)] tracking-wide font-Raleway italic ">
+            Aa jao <div className="h-[20px]"></div>
+            Gaand me lelo
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex p-7 mt-5">
+        <div className=" p-5 w-full">
+          <div className=" text-xl font-Raleway tracking-tight backdrop-blur-sm ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+            magni obcaecati adipisci omnis, assumenda suscipit quam architecto
+            quisquam ipsum aliquid rerum esse sint eum, soluta iste facilis,
+            ipsa laudantium. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Esse veritatis voluptates magni, ad corporis accusamus dolore
+            quisquam sint? Quaerat fuga ea possimus quos voluptates mollitia
+            quod corporis accusantium nisi in? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Quas pariatur consectetur sapiente,
+            maxime nihil possimus voluptatum molestias reprehenderit? Unde
+            laboriosam nihil ut libero voluptas inventore asperiores natus dolor
+            alias quo.
+          </div>
+          <div className="py-10">
+            <Link href="/menu">
+              <button className="flex justify-center items-center gap-2  h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-red-600  hover:shadow-xl p-2 tracking-wider">
+                View our Menu <FaArrowRight />
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="md:flex md:flex-col md:w-1/2 md:p-5 hidden">
+          <div className="h-1/2">
+            <img
+              src="https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Grilled-octopus-18ef1d3.jpg"
+              alt="buuter-chicken"
+              className="rounded-t-3xl"
+            />
+          </div>
+          <div className="flex h-1/2">
+            <div className="w-1/2">
+              <img
+                src="https://www.foodandwine.com/thmb/NNlZ_nd4CERT6lKaY8GzfmmhLXk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Essential-Greek-Foods-You-Need-to-Try-FT-3-BLOG1122-f9a9d1eb150a470ea29f5b4da8c3d1e7.jpg"
+                alt="idli-sambar"
+                className="rounded-bl-3xl"
+              />
+            </div>
+            <div className="w-1/2">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5RxD2iXpuvr7v-fmT9fpm7RUEBYlhxtRKgDGQeIb33g&s"
+                alt="pav-bhaji"
+                className="rounded-br-3xl"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="container mx-auto px-4 py-8">
+        <h2 className="text-3xl font-semibold text-center mb-6">
+          Some of our most popular delicacies
+        </h2>
+        <div className="flex flex-wrap gap-8 justify-around">
+          {indianFoods.map((food, index) => (
+            <RestaurantCard
+              key={index}
+              name={food.name}
+              description={food.description}
+              image={food.image}
+            />
+          ))}
+        </div>
       </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Page;
